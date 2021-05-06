097d6af04546ebb59582272b022c913eb558c4ca
Micah Gentzel <mgentzel@allebach.com>
2021-05-06 12:13:29 -0400

    initialize page templates

---
+++ page_foodservice.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Foodservice
+ */
+
+get_header();
+
+?>
+
+<div id="primary">
+
+    <main id="main" class="site_main dark_bg">
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
+            <header class="entry_header">
+                <?php the_title('<h1 class="entry_title">', '</h1>'); ?>
+            </header>
+            <div class="entry_content">
+                <?php the_content(); ?>
+            </div>
+        </article>
+
+    </main>
+
+</div>
+
+<?php
+get_footer();
